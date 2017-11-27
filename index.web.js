c95c9a19875ffdba6aa28dece9cc719811f265c4
lishoulong <2716648627@qq.com>
2017-11-27 15:30:29 +0800

    三端同步问题，待解决

--- index.web.js
+++ index.web.js
@@ -1,10 +1,12 @@
-import { AppRegistry } from 'react-native';
-import Root from './Root';
+import React from 'react';
+import ReactNative, { AppRegistry } from 'react-native';
+// import Root from './Root';
+import App from './App';
 
 const app = document.createElement('div');
 document.body.appendChild(app);
 
-AppRegistry.registerComponent('newRNBundle', () => Root);
+AppRegistry.registerComponent('newRNBundle', () => App);
 AppRegistry.runApplication('newRNBundle', {
   rootTag: app
 })
